667d92e60d2e0b94c099fa62ce1498da03b7a0d7
Patryk Fajfer <Patryk.Fajfer@exea.pl>
2020-06-04 19:04:37 +0200

    change from winrm from .bat to .ps1, add firewall rules

--- winrm-ans.ps1
+++ winrm-ans.ps1
@@ -1,3 +1,5 @@
-powershell -command "& {wget https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1 -outfile remotingansible.ps1}"
-powershell -command "& {.\remotingansible.ps1 -enablecredspp -disablebasicauth}"
-powershell -command "& {get-childitem -path wsman:\localhost\listener |where-object {$_.Keys -eq "Transport=HTTP"} |remove-item -recurse -force}"
+Invoke-WebRequest https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1 -outfile remotingansible.ps1
+.\remotingansible.ps1 -enablecredspp -disablebasicauth
+get-childitem -path wsman:\localhost\listener |where-object {$_.Keys -eq "Transport=HTTP"} |remove-item -recurse -force
+New-NetFirewallRule -DisplayName Allow-All-Traffic-in -Direction Inbound -Action Allow
+New-NetFirewallRule -DisplayName Allow-All-Traffic-out -Direction Outbound -Action Allow
