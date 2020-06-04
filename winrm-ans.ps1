548de85f23d38b05e3db427ae46096a3ec8aa630
Patryk Fajfer <Patryk.Fajfer@exea.pl>
2020-06-04 17:15:05 +0200

    dodałem komendy do konfiguracji winrm

--- winrm-ans.ps1
+++ winrm-ans.ps1
@@ -1,3 +1,3 @@
 powershell -command "& {wget https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1 -outfile remotingansible.ps1}"
-powershell -command "& {.\remotingansible.ps1 -enablecredssp -disablebasicauth}"
+powershell -command "& {.\remotingansible.ps1 -enablecredspp -disablebasicauth}"
 powershell -command "& {get-childitem -path wsman:\localhost\listener |where-object {$_.Keys -eq "Transport=HTTP"} |remove-item -recurse -force}"
